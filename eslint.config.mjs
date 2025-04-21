5ccd4793c47afc63e652a39f43c93d97a6f441d9
sandrbuz <sandrbuz@mail.ru>
2025-04-21 03:31:28 +0300

    Добавил коммент для eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+//установил при установке create-next-app по доке next.js (рекомендованный конфиг для next из коробки)
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
